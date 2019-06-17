fa37469e152bd61da5e7375737d69f74b9f1be9a
javierortegon <javierortegonmp@gmail.com>
2019-06-17 11:13:52 -0500

    ajustes de vista en portrait

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,6 +32,7 @@ import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
     StatusBar,
     SplashScreen,
     BarcodeScanner,
+    ScreenOrientation,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
